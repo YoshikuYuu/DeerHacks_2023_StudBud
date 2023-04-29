5b2e021a05e59fb50e563181a260bbf9565253b5
Alisha <alisha.hasan@live.ca>
2023-04-29 13:41:54 -0400

    Update database.py

--- database.py
+++ database.py
@@ -31,6 +31,8 @@ cursor.execute('''CREATE TABLE IF NOT EXISTS users (
 #     print(row)
 
 
+# bot event that initializes user in database if the user isn't already in the database
+# user is initialized with their name, an empty string as a task, and None as time
 @bot.event
 async def on_message(message):
     if message.author == bot.user:
@@ -51,6 +53,39 @@ async def on_message(message):
         user_id = f"{username}#{discriminator}"
         cursor.execute("INSERT INTO users (user_id, task) VALUES (?, '')", (user_id,))
         conn.commit()
+
+# bot command to record tasks; current format example -> !task "do laundry" 7:02
+# note that multiple tasks are stored in JSON format
+# bug in code about time storage that i'm currently fixing - alisha
+@bot.command(name='task')
+async def record_task(ctx, task: str, time_str: str):
+    try:
+        # remember to convert time_str into the appropriate format
+        time = datetime.strptime(str(time_str), '%I:%M')
+        time_formatted = time.strftime('%H:%M')
+        username = ctx.author.name
+        discriminator = ctx.author.discriminator
+        user_id = f"{username}#{discriminator}"
+
+        cursor.execute("SELECT task FROM users WHERE user_id=?", (user_id,))
+        result = cursor.fetchone()
+        if result is not None and result != '':
+            try:
+                current_tasks = json.loads(result[0])
+            except json.JSONDecodeError:
+                current_tasks = []
+        else:
+            current_tasks = []
+        current_tasks.append({"task": task, "time": time_formatted})
+        cursor.execute("REPLACE INTO users (user_id, task) VALUES (?, ?)", (user_id, json.dumps(current_tasks)))
+        conn.commit()
+
+        await ctx.send(f'Recorded task {task}. Reminder set for {time_formatted}.')
+    except ValueError as e:
+        await ctx.send(f'Error {e}. Invalid time format. Please use the format "HH:MM AM/PM".')
+    except json.JSONDecodeError as e:
+        print(f'Error {e}. JSON string: {result[0]}')
+        await ctx.send(f'Error {e}. Failed to load tasks from database.')
 
     # close database connection
     cursor.close()
